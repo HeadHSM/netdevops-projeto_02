d9b8b1376155bc174ad6c99914696b3723292677
Reidne Moreira <reidnemu@yahoo.com>
2026-04-18 12:54:44 -0300

    feat: main.py

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import scripts.login
 
 def main():
-    while True:
-        print("--- PAINEL PRINCIPAL ---")
-        print("1. Login")
-        print("2. Sair")
-        opcao = int(input("Selecione uma opção: "))
-
-        if opcao == 1:
-            
+    print("Ainda em construção")
 
 
 
